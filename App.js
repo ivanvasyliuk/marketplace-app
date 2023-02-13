f6b6c9e2749aa57a66a2b42a300f8c44fd9cd5f7
ivanvasyliuk <96949111+ivanvasyliuk@users.noreply.github.com>
2023-02-13 19:20:58 +0200

    making stucture

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import colors from "./src/styles/colors";
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.counter}>{count}</Text>
+      <View style={styles.buttonContainer}>
+        <Button
+          color={colors.primary}
+          onPress={() => setCount(count - 1)}
+          title="decrement"
+        />
+        <Button
+          color={colors.primary}
+          onPress={() => setCount(count + 1)}
+          title="increment"
+        />
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -13,8 +28,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    padding: 5,
+  },
+  counter: {
+    marginBottom: 10,
   },
 });
